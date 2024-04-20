af1d2770463b879610f3c925fb746f72d92a7682
Purnima Shrivastava <anuragshrivastava@Anurags-MacBook-Air.local>
2024-04-21 03:40:36 +0530

    schema updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+import { signIn } from "next-auth/react";
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <button onClick={(  )=>signIn()}>
+      SignIn
+      </button>
     </main>
   );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,59 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { AuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { db } from "./db";
-import Email from "next-auth/providers/email";
-
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
   },
-  pages: {
-    signIn: "/sign-in",
-  },
+  // pages: {
+  //   signIn: "/sign-in",
+  // },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     CredentialsProvider({
-      name:'Credentials',
-      credentials : {
-        email : {label:"Email", type:"email", placeholder="xyz@gmail.com",
-        password:{label:"password", type:"password"}
-         }
-      }
-    })
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "xyz@gmail.com" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+      
+          const user = await db.user.findFirst({
+            where: {
+              email: credentials?.email,
+            },
+          });
+          console.log(user);
+
+          if (!user) {
+            return null;
+          }
+
+          //if google login mail is used
+          if (user.password === undefined) {
+            return null;
+          }
+
+          if (user.password !== credentials?.password) {
+            return null;
+          }
+
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          };
+        
+      },
+    }),
   ],
   callbacks: {
     async session({ session, token }) {
